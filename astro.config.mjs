0b34d9a00a31711fdf0f28f1c33557109e47d87c
rr37 <ay870527@gmail.com>
2026-06-08 00:08:46 +0800

    chore: configure custom domain civictech.tw

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cooldongdong.github.io',
-  base: '/civictech.tw',
+  site: 'https://civictech.tw',
+  base: '/',
 });
